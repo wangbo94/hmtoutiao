3ecf524477234dff04bbf551dfded915c8859028
wangbo94 <1078577811@qq.com>
2019-10-25 20:55:23 +0800

    使用路由vue-router

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+// import Login from '@/views/login'
+
+Vue.use(VueRouter)
+export default new VueRouter({
+  routes: [{
+    // path: '/login',
+    // components: Login
+
+  }]
+})
