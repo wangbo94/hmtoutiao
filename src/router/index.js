2adf33505e3ada85bd0371e963baf1339a95f2a3
wangbo94 <1078577811@qq.com>
2019-10-26 21:11:05 +0800

    晚上放学

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,15 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 export default new VueRouter({
   routes: [{
     path: '/login',
     component: Login
-
+  }, {
+    path: '/',
+    component: Home
   }]
 })
---
+++ src/views/home/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <el-container class="home-container">
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main></el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang='less'>
+.home-container {
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .el-aside {
+    background: #002233;
+  }
+  .el-header {
+    border-bottom: 1px #ccc solid;
+  }
+}
+</style>
